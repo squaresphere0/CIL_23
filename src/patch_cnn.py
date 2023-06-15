f04f1638ea03aabf0d8c63cf4fc2f22902caaf6f
Jonathan Manz <manzjo@student.ethz.ch>
2023-06-15 16:05:58 +0200

    patch-cnn training working...

---
+++ src/patch_cnn.py
@@ -0,0 +1,33 @@
+import torch
+from torch import nn
+
+class PatchCNN(nn.Module):
+    # simple CNN for classification of patches
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3, padding=1),
+                                 nn.ReLU(),
+                                 nn.MaxPool2d(2, 2),
+                                 nn.BatchNorm2d(16),
+                                 nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+                                 nn.ReLU(),
+                                 nn.MaxPool2d(2, 2),
+                                 nn.BatchNorm2d(32),
+                                 nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),
+                                 nn.ReLU(),
+                                 nn.MaxPool2d(2, 2),
+                                 nn.BatchNorm2d(64),
+                                 nn.Dropout(0.5),
+                                 nn.Flatten(),
+                                 nn.Linear(256, 10),
+                                 nn.ReLU(),
+                                 nn.Dropout(0.5),
+                                 nn.Linear(10, 1),
+                                 nn.Sigmoid())
+
+    def forward(self, x):
+        return self.net(x)
+
+def accuracy_fn(y_hat, y):
+    # computes classification accuracy
+    return (y_hat.round() == y.round()).float().mean()
---
+++ src/training_loop.py
@@ -0,0 +1,87 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from torch.utils.tensorboard import SummaryWriter
+from tqdm.notebook import tqdm
+
+def show_val_samples(x, y, y_hat, segmentation=False):
+    # training callback to show predictions on validation set
+    imgs_to_draw = min(5, len(x))
+    if x.shape[-2:] == y.shape[-2:]:  # segmentation
+        fig, axs = plt.subplots(3, imgs_to_draw, figsize=(18.5, 12))
+        for i in range(imgs_to_draw):
+            axs[0, i].imshow(np.moveaxis(x[i], 0, -1))
+            axs[1, i].imshow(np.concatenate([np.moveaxis(y_hat[i], 0, -1)] * 3, -1))
+            axs[2, i].imshow(np.concatenate([np.moveaxis(y[i], 0, -1)]*3, -1))
+            axs[0, i].set_title(f'Sample {i}')
+            axs[1, i].set_title(f'Predicted {i}')
+            axs[2, i].set_title(f'True {i}')
+            axs[0, i].set_axis_off()
+            axs[1, i].set_axis_off()
+            axs[2, i].set_axis_off()
+    else:  # classification
+        fig, axs = plt.subplots(1, imgs_to_draw, figsize=(18.5, 6))
+        for i in range(imgs_to_draw):
+            axs[i].imshow(np.moveaxis(x[i], 0, -1))
+            axs[i].set_title(f'True: {np.round(y[i]).item()}; Predicted: {np.round(y_hat[i]).item()}')
+            axs[i].set_axis_off()
+    plt.show()
+
+def train(train_dataloader, eval_dataloader, model, loss_fn, metric_fns, optimizer, n_epochs):
+    # training loop
+    logdir = './tensorboard/net'
+    writer = SummaryWriter(logdir)  # tensorboard writer (can also log images)
+
+    history = {}  # collects metrics at the end of each epoch
+
+    for epoch in range(n_epochs):  # loop over the dataset multiple times
+
+        # initialize metric list
+        metrics = {'loss': [], 'val_loss': []}
+        for k, _ in metric_fns.items():
+            metrics[k] = []
+            metrics['val_'+k] = []
+
+        pbar = tqdm(train_dataloader, desc=f'Epoch {epoch+1}/{n_epochs}')
+        # training
+        model.train()
+        for (x, y) in pbar:
+            optimizer.zero_grad()  # zero out gradients
+            y_hat = model(x)  # forward pass
+            loss = loss_fn(y_hat, y)
+            loss.backward()  # backward pass
+            optimizer.step()  # optimize weights
+
+            # log partial metrics
+            metrics['loss'].append(loss.item())
+            for k, fn in metric_fns.items():
+                metrics[k].append(fn(y_hat, y).item())
+            pbar.set_postfix({k: sum(v)/len(v) for k, v in metrics.items() if len(v) > 0})
+
+        # validation
+        model.eval()
+        with torch.no_grad():  # do not keep track of gradients
+            for (x, y) in eval_dataloader:
+                y_hat = model(x)  # forward pass
+                loss = loss_fn(y_hat, y)
+                
+                # log partial metrics
+                metrics['val_loss'].append(loss.item())
+                for k, fn in metric_fns.items():
+                    metrics['val_'+k].append(fn(y_hat, y).item())
+
+        # summarize metrics, log to tensorboard and display
+        history[epoch] = {k: sum(v) / len(v) for k, v in metrics.items()}
+        for k, v in history[epoch].items():
+          writer.add_scalar(k, v, epoch)
+        print(' '.join(['\t- '+str(k)+' = '+str(v)+'\n ' for (k, v) in history[epoch].items()]))
+        show_val_samples(x.detach().cpu().numpy(), y.detach().cpu().numpy(), y_hat.detach().cpu().numpy())
+
+    print('Finished Training')
+    # plot loss curves
+    plt.plot([v['loss'] for k, v in history.items()], label='Training Loss')
+    plt.plot([v['val_loss'] for k, v in history.items()], label='Validation Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+    plt.show()
