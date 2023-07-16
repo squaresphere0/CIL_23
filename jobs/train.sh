657e53f1e89d061030ba6157c2322ce65ea8a9a6
Petr Koludarov <mrpetrkol2@gmail.com>
2023-07-16 10:07:09 +0200

    train.sh script

---
+++ jobs/train.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=20:30:00
+#SBATCH --mem-per-cpu=32g
+#SBATCH --job-name=pixel_cnn
+#SBATCH --output=./out/pixel_cnn.out
+#SBATCH --error=./out/pixel_cnn.err
+#SBATCH --gpus=1
+#SBATCH --gres=gpumem:20g
+
+cd ..
+
+export SETUPTOOLS_USE_DISTUTILS=stdlib
+
+module load gcc/6.3.0 python_gpu/3.8.5 eth_proxy
+source venv/bin/activate
+
+python src/main.py
