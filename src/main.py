544553d1a0abce7bb3c42f6b2fa658db385e9208
Jonathan Manz <manzjo@student.ethz.ch>
2023-07-21 12:29:22 +0200

    testing 0.5 dropout model.
    ...

--- src/main.py
+++ src/main.py
@@ -17,14 +17,26 @@ original_dataset = dataloader.LazyImageDataset(
     'Datasets/ethz-cil-road-segmentation-2023/metadata.csv',
     size = (100,100))
 
-loader = DataLoader(original_dataset, 32, shuffle=True)
+loader = DataLoader(original_dataset, 4, shuffle=True)
 
 layers = [7] + [3 for _ in range(15)]
 
-model = conditionalPixelCNN(20,1,4, layers, noise=1).to(device)
+model = conditionalPixelCNN(20,1,4, layers, noise=0.5).to(device)
 
 optimizer = torch.optim.Adam(model.parameters())
 
-losses = conditionalPixelCNN.training(model,loader,optimizer, 200,
-                                      'non_auto_regressive')
+medium_noise_model = torch.load('model/medium_noise_model.pt',
+                                map_location=device)
+losses = medium_noise_model['loss_history']
+model.load_state_dict(medium_noise_model['model_state_dict'])
+
+
+for images, masks in loader:
+    model.eval()
+    generated = model.generate_samples(4, 100, images)
+    visualize_images(masks, generated)
+'''
+losses = conditionalPixelCNN.training(model,loader,optimizer, 2000,
+                                      'noise_0.5_epoch_2000')
+                                      '''
 
