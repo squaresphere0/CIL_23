a1f723782a618ee8ee72624e9ab8c3bc357b10d3
Hygster <august.b.ronberg@gmail.com>
2023-07-24 10:49:02 +0200

    Submission pipeline working

--- src/main.py
+++ src/main.py
@@ -8,8 +8,19 @@ import matplotlib.pyplot as plt
 import pixel_cnn
 from pixel_cnn import conditionalPixelCNN
 from pixel_cnn import visualize_images
-from pixel_cnn import shift_mask
 import dataloader
+
+def visualize_images(original, reconstructed):
+    fig, axes = plt.subplots(nrows=2, ncols=4, figsize=(12, 6))
+    for i in range(4):
+        axes[0, i].imshow(original[i].permute(1, 2, 0).squeeze())
+        axes[0, i].axis('off')
+        axes[0, i].set_title("Original")
+        axes[1, i].imshow(reconstructed[i].detach().numpy().squeeze(), cmap='gray')
+        axes[1, i].axis('off')
+        axes[1, i].set_title("Reconstructed")
+    plt.tight_layout()
+    plt.show()
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu' 
 
@@ -33,11 +44,9 @@ model.load_state_dict(medium_noise_model['model_state_dict'])
 
 with torch.no_grad():
     model.eval()
-    for image, mask in loader:
-        pred = model.inference_by_iterative_refinement(0.5, 100, BATCHSIZE, 100, image)
-        visualize_images(image.movedim(1,3), pred)
-        break
-
-#losses = conditionalPixelCNN.training(model,loader,optimizer, 10,
-#                                      'finetuned_10epochs')
-
+    generated = model.generate_samples(4, 100, images)
+    visualize_images(masks, generated)
+'''
+losses = conditionalPixelCNN.training(model,loader,optimizer, 2000,
+                                      'noise_0.5_epoch_2000')
+                                      '''
