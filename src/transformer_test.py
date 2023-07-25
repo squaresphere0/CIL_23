454fd90ced8e4196178cdc2533a12dfb4221a858
Petr Koludarov <mrpetrkol2@gmail.com>
2023-07-25 15:16:03 +0200

    refactor inference script

--- src/transformer_test.py
+++ src/transformer_test.py
@@ -3,7 +3,7 @@ from sklearn.metrics import f1_score
 import matplotlib.pyplot as plt
 import numpy as np
 
-from transformer_with_unet import ViTUNet, UnetDecoder, ImageDataset
+from just_a_transformer import PixelSwinT, ImageDataset
 from torch.utils.data import DataLoader
 from torchvision import transforms
 
@@ -12,7 +12,7 @@ import dataloader
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # Load your saved model
-model = torch.load('model/almost_my_transformer_with_proper_unet.pt', map_location=torch.device('cpu'))
+model = torch.load('model/resonant_lungfish_7960_just_a_tranformer_epoch_300.pt', map_location=torch.device('cpu'))
 
 train_dataset = ImageDataset('data/training', 'cuda' if torch.cuda.is_available() else 'cpu')
 val_dataset = ImageDataset('data/validation', 'cuda' if torch.cuda.is_available() else 'cpu')
@@ -23,34 +23,41 @@ y_true = []
 y_pred = []
 
 with torch.no_grad():
+    sum_f1 = 0
     for i, (image, label) in enumerate(val_dataloader):
-        resize = transforms.Resize((224, 224))
+        print(f'Iteration: {i}')
+        # resize = transforms.Resize((224, 224))
 
-        image = resize(image).to(device)
-        label = resize(label).to(device)
+        image = image.to(device)
+        label = label.to(device)
 
         # image = image.to(device)
         # label = label.view(-1).to(device)
 
-        outputs = model(image)
+        outputs, _ = model(image)
 
         # Apply a threshold of 0.5: above -> 1, below -> 0
-        preds = outputs # (outputs > 0.5).float()
+        threshold = 0.25
+        preds = outputs # (outputs > 0.25).float()
+        # print(label.shape, preds.shape)
+        sum_f1 += f1_score(label.view(-1).cpu().numpy(), (outputs > threshold).view(-1).cpu().numpy(), average='binary')  # binary case
+
         # print(torch.nonzero(preds))
+        np_threshold = np.squeeze((outputs > threshold).float().numpy())
         np_preds = np.squeeze(preds.numpy())
         np_label = np.squeeze(label.numpy())
         np_image = np.transpose(np.squeeze(image.numpy()), (1, 2, 0))
-        print(np_image.shape)
+        # print(np_image.shape)
 
         # preds = preds.view(-1)
 
-        y_true.extend(label.cpu().numpy())
-        y_pred.extend(preds.cpu().numpy())
-        print(label.shape, preds.shape)
+        # y_true.extend(label.cpu().numpy())
+        # y_pred.extend(preds.cpu().numpy())
+        # print(label.shape, preds.shape)
         
-        fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+        fig, axs = plt.subplots(1, 4, figsize=(20, 5))
 
-       # Display np_image1
+        # Display np_image1
         axs[0].imshow(np_image, cmap='gray')
         axs[0].axis('off')
 
@@ -59,15 +66,21 @@ with torch.no_grad():
         axs[1].axis('off')
 
         # Display np_image3
-        axs[2].imshow(np_preds, cmap='gray')
+        axs[2].imshow(np_threshold, cmap='gray')
         axs[2].axis('off')
+
+        # Display np_image4
+        axs[3].imshow(np_preds, cmap='gray')
+        axs[3].axis('off')
+
         # Save the figure with both subplots
         plt.subplots_adjust(wspace=0, hspace=0)
         plt.tight_layout()
-        plt.savefig(f'preds/combined_{i}.png', bbox_inches='tight', pad_inches=0)
+        plt.savefig(f'preds/_manual_validation_{i}.png', bbox_inches='tight', pad_inches=0)
         plt.close()
+
+    print(f'Avg F1 score: {sum_f1 / len(val_dataloader)}')
+
 
 
 # Compute F1 score
-f1 = f1_score(y_true, y_pred, average='binary')  # binary case
-print(f"F1 Score: {f1}")
