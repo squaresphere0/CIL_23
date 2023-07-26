fix data normalization

src/just_a_transformer.py
@@ -240,12 +240,8 @@ class ImageDataset(torch.utils.data.Dataset):
         self.n_samples = len(self.x)
 
     def _preprocess(self, x, y, angle=0):
-        transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
+        transform = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         x = transform(x)
-        y = transform(y)
         if not self.preprocessing or not self.is_train:
             return x, y
         # to keep things simple we will not apply transformations to each sample,