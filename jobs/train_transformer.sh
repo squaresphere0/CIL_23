5a9a2e7ff3834fc9ef05b726629384f4fb2d2778
Petr Koludarov <mrpetrkol2@gmail.com>
2023-07-27 16:41:08 +0200

    num_workers=8

--- jobs/train_transformer.sh
+++ jobs/train_transformer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
 #SBATCH --time=3:30:00
 
 #SBATCH --mem-per-cpu=32g
 #SBATCH --gpus=1
-#SBATCH --gres=gpumem:20g
+#SBATCH --gres=gpumem:40g
 
 #SBATCH --job-name=just_a_tr
 #SBATCH --output=./out/just_a_tr.out
